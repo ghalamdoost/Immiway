9641b974aec60d7fb416ef256584560e7310e5f0
ghalamdoost.shahab <ghalamdoost.shahab@live.com>
2021-08-08 00:09:02 -0400

    commit

--- app_server/routes/anonymous.js
+++ app_server/routes/anonymous.js
@@ -3,10 +3,10 @@ var router = express.Router();
 const ctrlAnonymous = require('../controllers/anonymous');
 
 /* GET home page. */
-router.get('/', ctrlAnonymous.home);
-router.get('/home', ctrlAnonymous.home);
-router.get('/aboutus',ctrlAnonymous.aboutUs)
-router.get('/contactus',ctrlAnonymous.contactUs)
+// router.get('/', ctrlAnonymous.home);
+// router.get('/home', ctrlAnonymous.home);
+// router.get('/aboutus',ctrlAnonymous.aboutUs)
+// router.get('/contactus',ctrlAnonymous.contactUs)
 
 
 module.exports = router;
